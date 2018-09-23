8507ac3321d1d4f8bec4ef031e5080bf2f558b42
johnsonc8415 <43526157+johnsonc8415@users.noreply.github.com>
2018-09-23 19:02:36 -0400

    Add files via upload
    Second Upload

---
+++ P2HW1_CelsiusConverter_ChrisJohnson.py
@@ -0,0 +1,15 @@
+#P2HW1 Celsius Fahrenheit Converter
+#9/23/18
+#CTI-110 P2HW1- Celsius Fahrenheit Converter
+#Chris Johnson
+#
+#insert the temperature
+#convert from celsius to fahrenheit
+#get temperature in fahrenheit
+celciustemperature = float(input('Enter temperature in celcius'))
+
+fahrenheittemperature = (( 9 / 5 ) * celciustemperature) + 32
+
+print( fahrenheittemperature )
+
+
