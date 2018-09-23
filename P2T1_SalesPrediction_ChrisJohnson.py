1ad83e8b3eee7b3c3160db24817cfaba6b01182b
johnsonc8415 <43526157+johnsonc8415@users.noreply.github.com>
2018-09-23 18:58:08 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_ChrisJohnson.py
@@ -0,0 +1,11 @@
+#Tutorial to python sales prediction
+#9/23/18
+#CTI-110 P2T1-Sales Prediction
+#Chris Johnson
+#
+total_sales = float(input('Enter the projected sales: '))
+
+profit = total_sales * 0.23
+
+print('The profit is $', format(profit, ',.2f'))
+
