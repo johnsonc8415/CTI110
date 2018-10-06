67c366158af29647feb7d5fb0c42beb5e374d08e
johnsonc8415 <43526157+johnsonc8415@users.noreply.github.com>
2018-10-06 01:25:48 -0400

    Add files via upload

---
+++ P3LAB_Johnson.py1.py
@@ -0,0 +1,45 @@
+# Debugging Lab
+# Chris Johnson
+#CTI-110 P3LAb
+#10/5/2018
+#Chris Johnson
+#
+
+def main():
+    # This program gets a numeric test score from the
+    # user and displays the corresponding letter grade.
+    # Variables to represent the grade thresholds
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60
+
+    #Get a test score from the user.
+    score = int(input('Enter your test score:'))
+
+    # Determine the grade.
+
+    if score >= A_score:
+        print('Your grade is: A.')
+    elif score >= B_score:
+        print( ' Your grade is B. ')
+    elif score >= C_score:
+        print( 'Your grade is C. ')
+    elif score >= D_score:
+        print( 'Your grade is D.')
+    else:
+        print( 'Your grade is F.')
+
+            
+        
+
+                           
+
+
+
+
+
+
+
+# program start
+main()
