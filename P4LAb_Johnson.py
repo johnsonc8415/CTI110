3b841ecda39b669462ab2f359bba3b9c7ac07b80
johnsonc8415 <43526157+johnsonc8415@users.noreply.github.com>
2018-10-29 00:18:51 -0400

    Add files via upload
    P4HW4

---
+++ P4LAb_Johnson.py
@@ -0,0 +1,23 @@
+import turtle
+
+chris = turtle.Turtle()
+chris.shape("turtle")
+chris.speed(500)
+
+chris.color("purple")
+
+for i in [0,1,2,3,4,5,6,7,8,9,10,11]:
+    chris.left(90)
+    chris.forward(100)
+    chris.forward(-40)
+    chris.left(40)
+    chris.forward(30)
+    chris.forward(-30)
+    chris.right(80)
+    chris.forward(30)
+    chris.forward(-30)
+    chris.left(40)
+    chris.forward(-60)
+    chris.right(60)
+    
+
