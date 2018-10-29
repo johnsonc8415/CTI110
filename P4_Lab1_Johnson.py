4a8daaf9fb7c562c325770cf662471e90c63fa6f
johnsonc8415 <43526157+johnsonc8415@users.noreply.github.com>
2018-10-29 00:16:02 -0400

    Add files via upload
    P4LAB1

---
+++ P4_Lab1_Johnson.py
@@ -0,0 +1,17 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+wn.title("Tess & Alex")
+tess = turtle.Turtle()
+tess.color("hotpink")
+tess.pensize(5)
+alex = turtle.Turtle()
+for i in[0,1,2,3]:
+    alex.forward(50)
+    alex.left(90)
+for i in[0,1,2,3,4,5]:
+    tess.forward(80)
+    tess.left(120)
+for i in[0,1]:
+    tess.right(180)
+    tess.forward(80)
